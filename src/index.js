4d768309b4df18532f5cf56dc24feb7bbf42cafd
Nuri1977 <nurilacka@gmail.com>
2022-01-25 13:08:22 +0100

    Post data to API implented

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import './styles/style.css';
 
 const btnScores = document.getElementById('btn-scores');
 
-const getScores = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dCWnK7FBkgPC2MczagXu/score/')
+const displaySucces = () => (console.log('meseage succes'));
+const displayError = (err) => (console.log(err));
+
+const getScores = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dCWnK7FBkgPC2MczagXu/scores/')
   .then((response) => response.json());
 
 const renderScores = async () => {
@@ -18,10 +21,43 @@ const renderScores = async () => {
 
         scoreList.innerHTML += listItem;
       });
-    })
-    .catch((error) => console.log(error));
+    });
 };
 
 btnScores.addEventListener('click', renderScores);
+
+const postScore = (nameInput, scoreInput) => {
+  const data = {
+    user: nameInput,
+    score: scoreInput,
+  };
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dCWnK7FBkgPC2MczagXu/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(((response) => response.json()))
+    .then((data) => {
+      if (data.result) {
+        displaySucces();
+      }
+    })
+    .catch((error) => displayError(error));
+};
+
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+const btnSubmit = document.getElementById('btn-submit');
+
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  postScore(nameInput.value, scoreInput.value);
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
 
 renderScores();
