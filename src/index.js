ed0d2b84db7378ab13867cd51feccf09501a863f
Nuri1977 <nurilacka@gmail.com>
2022-01-25 11:32:22 +0100

    Get data from the Api and render it on UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './styles/style.css';
+
+const btnScores = document.getElementById('btn-scores');
+
+const getScores = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dCWnK7FBkgPC2MczagXu/score/')
+  .then((response) => response.json());
+
+const renderScores = async () => {
+  await getScores()
+    .then((data) => data.result)
+    .then((scores) => {
+      const scoreList = document.getElementById('score-list');
+      scoreList.innerHTML = '';
+      scores.forEach((score) => {
+        const listItem = `
+        <li>${score.user}: ${score.score}</li>
+        `;
+
+        scoreList.innerHTML += listItem;
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+btnScores.addEventListener('click', renderScores);
+
+renderScores();
